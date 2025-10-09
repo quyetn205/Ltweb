afffbed1a02c2c8ea499562fdff28ceac8736c8b
quyetn205 <ngominhquyet4@gmail.com>
2025-10-09 20:52:16 +0700

    upcode

---
+++ tvcday08cf/tvcday08cf/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace tvcday08cf.Models
+{
+    [Table(name: "Category")]
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool isActive { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
---
+++ tvcday08cf/tvcday08cf/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tvcday08cf.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
